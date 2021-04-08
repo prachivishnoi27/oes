b8d2c0dc0f1f5824512168595ea8629958720b15
Prachi Vishnoi <prachi27vishnoi@gmail.com>
2021-04-08 23:37:07 +0530

    Homepage done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,3 @@
-import "./Home.css";
 import React from "react";
 import UnAuthHeader from "./Headers/UnAuthHeader";
 
@@ -11,6 +10,37 @@ const Home = () => {
       <UnAuthHeader />
       <div className="top-center">
         <h1>Online Examination system</h1>
+      </div>
+      <div className="ui divider"></div>
+      <div style={{ background: "white", padding: "10px", fontSize: "16px" }}>
+        <p>
+          In this system students are asked to select answers from multiple
+          options given for a single question. Likewise, there are several
+          questions which appear in the students' systems. The questions and
+          multiple options are saved in a database along with desired answers.
+          Typically, a student can edit an answer after saving it, however
+          editing cannot be done after submitting the answer. Another user is
+          also there - administrator. The administrator can create, modify and
+          delete questions and accordingly, the question is updated in the
+          system.
+        </p>
+      </div>
+      <div
+        className="ui borderless inverted menu"
+        style={{
+          flexShrink: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: 'white',
+          width: "83vw",
+          height: "10vh",
+          marginTop: "10px",
+          bottom: "0px",
+          position: "absolute",
+        }}
+      >
+        <div>Made by Prachi Vishnoi</div>
       </div>
     </div>
   );
